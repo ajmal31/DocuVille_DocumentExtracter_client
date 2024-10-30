11a47fe386b361fcd2f0f0dd1ac32aa9946ffbf4
ajmal31 <ajmalmuhammed846@gmail.com>
2024-10-30 15:01:23 +0530

    Refactor : readme

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# For Running project on your system
+ - git clone https://github.com/ajmal31/DocuVille_DocumentExtracter_client.git
+ - npm install 
+ - npm run dev ( Dev Mode) -> http://localhost:5173/
+ - npm run build
+ - serve -s dist http://localhost:3000 ( builded file)
--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,28 @@ import { useState } from "react";
 function App() {
   const [extractedDetails, setExtractedDetails] = useState("");
   const [file, setFile] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   // Handle Upload
   const handleUpload = (e) => {
     setFile(e.target.files[0]);
   };
-  
+
   // POST Request
   const postData = async (data) => {
     try {
+      setExtractedDetails("");
+      setLoading(true);
       const response = await axios.post(`${BASE_URL}upload`, data);
+      setLoading(false);
       setExtractedDetails(response.data);
     } catch (error) {
       console.log("eror", error.message);
+      setError(true);
     }
   };
-  
+
   // Hanlde Submit
   const handleSubmit = () => {
     if (file.length <= 0) alert("please upload file");
@@ -32,8 +38,11 @@ function App() {
   };
   return (
     <>
-      <div className="p-20 border border-black flex gap-5 ">
-        <div className="w-1/3 border border-black">
+      <div className="p-20 border border-black flex gap-5 h-screen bg-green-600  text-white">
+        <div className="w-1/3 ">
+          <label htmlFor="" className="text-xl mb-5">
+            Upload you driving License
+          </label>
           <input type="file" onChange={handleUpload} />
           <button
             className="border bg-blue-500 px-2 py-1 rounded-md"
@@ -42,15 +51,19 @@ function App() {
             submit
           </button>
         </div>
-        {extractedDetails ? (
-          <div className="w-2/3">
-            <p>
-             {extractedDetails}
-            </p>
+        {error ? (
+          <h1>Error in loading data.. Refresh🌀🌀</h1>
+        ) : extractedDetails ? (
+          <div className="w-2/3 ">
+            <p>{extractedDetails}</p>
           </div>
+        ) : loading ? (
+          <h1>Loading....</h1>
         ) : (
           ""
         )}
+
+        {}
       </div>
     </>
   );
